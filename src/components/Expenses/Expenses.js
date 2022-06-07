60604055c202578cccccd788d162f7cabe628c26
Skwieru <mskwieru@gmail.com>
2022-06-07 15:55:21 +0200

    Adding filter to the Expense Tracker

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,36 +1,49 @@
-import React from "react";
+import { React, useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
+  const [filterState, setFilterState] = useState("2020");
+  const liftFilterState = (selectedYear) => {
+    setFilterState(selectedYear);
+    console.log(selectedYear, "from higher");
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        id={props.expenses[0].id}
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        id={props.expenses[1].id}
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        id={props.expenses[2].id}
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        id={props.expenses[3].id}
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
+    <>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filterState}
+          liftFilterState={liftFilterState}
+        />
+        <ExpenseItem
+          id={props.expenses[0].id}
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          id={props.expenses[1].id}
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          id={props.expenses[2].id}
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          id={props.expenses[3].id}
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </>
   );
 };
 
